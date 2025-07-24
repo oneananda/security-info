e7b7a2a36339730a1de53c0e7665082ffba3d48d
oneananda <anandathirumurugan@gmail.com>
2025-07-24 21:52:00 +0530

    Vulnerable Code (Broken Access Control)

---
+++ OWASP Top 10/Code_Examples/UsersController.cs
@@ -0,0 +1,25 @@
+﻿// Description: This code defines a UsersController in an ASP.NET Core application that has a vulnerability due to a missing access check on an endpoint.
+// Vulnerable Code (Broken Access Control)
+using Microsoft.AspNetCore.Mvc;
+using MyApplication.Services.Interfaces;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+	private readonly IUserService _userService;
+
+	public UsersController(IUserService userService)
+	{
+		_userService = userService;
+	}
+
+	// ❌ This endpoint is vulnerable: no access check
+	[HttpGet("all")]
+	public IActionResult GetAllUsers()
+	{
+		var users = _userService.GetAllUsers();
+		return Ok(users);
+	}
+}
